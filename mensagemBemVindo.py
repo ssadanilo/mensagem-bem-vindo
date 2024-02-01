e4dc50fa0dffe21586b62dc84bb2c33102ce42d1
Dev_Danilo <ssadanilo@hotmail.com>
2024-02-01 17:29:34 -0300

    Update mensagemBemVindo.py

--- mensagemBemVindo.py
+++ mensagemBemVindo.py
@@ -1,5 +1,5 @@
-nome = input("Digite seu nome: ")
-sobrenome = input("Digite seu nome: ")
-bebida = input("O que você gostaria de beber agora? ")
+nome = input('Digite seu nome: ')
+sobrenome = input('Digite seu nome: ')
+bebida = input('O que você gostaria de beber agora? ')
 
-print(f"Olá {nome} {sobrenome}! Meu nome é Paula Vadão. É um prazer receber você em nosso Hotel.\nAceita beber {bebida} enquanto faço seu checkin?")
+print(f'Olá {nome} {sobrenome}! Meu nome é Paula Vadão. É um prazer receber você em nosso Hotel.\nAceita beber {bebida} enquanto faço seu checkin?')
